8fdbc5bd70fd86853071835926f39b486c9665d3
parthao <parthaohack@gmail.com>
2023-10-12 10:43:07 +0530

    Added new Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { XyzComponent } from './xyz/xyz.component';
+import { ImageBlockComponent } from './image-block/image-block.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent
+    XyzComponent,
+    ImageBlockComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/image-block/image-block.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-block',
+  templateUrl: './image-block.component.html',
+  styleUrls: ['./image-block.component.css']
+})
+export class ImageBlockComponent {
+
+}
